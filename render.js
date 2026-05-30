aa93db43c65b18654738fba9cb90ba4227838746
deltamemories <maxenot123@gmail.com>
2026-05-30 08:57:29 +0200

    feat: add theme name in output filename
    add theme check, refactor code

--- render.js
+++ render.js
@@ -3,16 +3,31 @@ import handlebars from 'handlebars';
 import puppeteer from 'puppeteer';
 
 
+const availableThemes = Object.freeze({
+	light: 'light',
+	dark: 'dark',
+	oled: 'oled',
+})
+
+
 async function renderSchedule() {
+	let browser
 	try {
 		const templateHtml = await fs.readFile('./template.html', 'utf8');
 		const data = JSON.parse(await fs.readFile('./data.json', 'utf8'));
-		const scale_factor = data['scale_factor'];
+		const scaleFactor = data['scale_factor'];
+
+		if (!Object.values(availableThemes).includes(data['theme'])) {
+			console.error("Missing theme parameter in configuration");
+			return
+		}
+
+		const theme = data['theme'];
 
 		const template = handlebars.compile(templateHtml);
 		const renderedHtml = template(data);
 
-		const browser = await puppeteer.launch();
+		browser = await puppeteer.launch();
 		const page = await browser.newPage();
 
 		await page.setContent(renderedHtml, {waitUntil: 'networkidle0'});
@@ -28,16 +43,17 @@ async function renderSchedule() {
 			return {x, y, width, height};
 		})
 
-		if (scheduleBounds) {
+		if (!scheduleBounds) {
+			console.error("Can't find schedule element");
+		} else {
 			const now = new Date();
 			const dateString = `${(now.getHours() + 1).toString().padStart(2, '0')}_${(now.getMinutes() + 1).toString().padStart(2, '0')}_${(now.getSeconds() + 1).toString().padStart(2, '0')}_${now.getDate().toString().padStart(2, '0')}_${(now.getMonth() + 1).toString().padStart(2, '0')}_${now.getFullYear()}`;
-			const fileName = `./rendered/schedule_${dateString}.png`;
-
+			const fileName = `./rendered/schedule_${dateString}_${theme}.png`;
 
 			await page.setViewport({
 				width: Math.ceil(scheduleBounds.width),
 				height: Math.ceil(scheduleBounds.height),
-				deviceScaleFactor: scale_factor,
+				deviceScaleFactor: scaleFactor,
 			})
 
 			try {
@@ -53,14 +69,14 @@ async function renderSchedule() {
 				fullPage: true,
 			});
 			console.log('Page rendered successfully');
-		} else {
-			console.log("Can't find schedule element");
 		}
-
-		await browser.close();
 
 	} catch (error) {
 		console.error("Render error:", error);
+	} finally {
+		if (browser) {
+			await browser.close()
+		}
 	}
 }
 
